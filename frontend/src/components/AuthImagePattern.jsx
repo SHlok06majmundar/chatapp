3be8bf1caaa235496811736f9c6abaf23e5a6018
shlok <majmundarshlok06@gmail.com>
2025-02-01 13:04:39 +0530

    complete

---
+++ frontend/src/components/AuthImagePattern.jsx
@@ -0,0 +1,17 @@
+const AuthImagePattern = ({ title, subtitle }) => {
+  return (
+    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
+      <div className="max-w-md text-center">
+        <img 
+          src="/logo_big.png" 
+          alt="Auth Logo" 
+          className="mb-8 max-w-full h-auto"
+        />
+        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+        <p className="text-base-content/60">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AuthImagePattern;
